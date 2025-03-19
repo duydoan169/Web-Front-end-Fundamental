378fc1bd2e7fc207ac0a0212641796a1a39b87f1
duydoan169 <hanh250470@gmail.com>
2025-03-20 01:51:56 +0700

    bai tap buoi 31

---
+++ session31/baitap/bai1.js
@@ -0,0 +1,6 @@
+function show(){
+    document.getElementsByTagName("h1")[0].style.display = "block";
+}
+function hide(){
+    document.getElementsByTagName("h1")[0].style.display = "none";
+}
---
+++ session31/baitap/bai2.js
@@ -0,0 +1,5 @@
+function darkMode(){
+    h1E=document.getElementsByTagName("h1")[0].classList.toggle('black');
+    pE=document.getElementsByTagName("p")[0].classList.toggle('black');
+    bodyE=document.getElementsByTagName("body")[0].classList.toggle('black');
+}
